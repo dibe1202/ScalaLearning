992666f9d0f78afd4ce9ac860114ef303974a3bd
dibe <wanghuanan2@longigroup.com>
2020-02-24 10:13:22 +0800

    操作：ScalaLearning--快学scala教程学习--HelloScalaWorld 日期：2020-02-20 提交人：王华男

---
+++ ScalaForTheImpatient/src/main/scala/HelloScala.scala
@@ -0,0 +1,12 @@
+/**
+ * @ project:  Scalalearning
+ * @ package:  
+ * @ author:   whn
+ * @ date:     2020-2-24 10:10
+ * @ Function:    
+ */
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala World!")
+  }
+}
